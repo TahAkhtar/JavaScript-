f1997d011c9045be433da40c2f67a9f743500136
Tahseen Akhtar <tahseenakhtar869@gmail.com>
2024-03-18 14:32:57 +0000

    Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = 99
+let age = "45a"
+console.log(typeof score)
+console.log(typeof(score))
+let valueInNumber = Number(age)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// Conversion "33"=> 33
+// 333xyz=> NaN
+// True=> 1
+//  Conversion of Boolean 1 into True
+// "" => false
+// "Lpu"=>True
