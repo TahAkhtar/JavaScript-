b0cb5e03871e9289bd2973ac343fdaf3bfe166e4
Tahseen Akhtar <tahseenakhtar869@gmail.com>
2024-03-18 13:50:48 +0000

    Datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 34568
+let accountMail="JaggaJasus786@gmail.com"
+var accountPassword = "345678"
+let accountCity="Pune"
+let accountState;
+console.table([accountId,accountMail,accountPassword,accountCity,accountState]);
+// Single Line Comment
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,5 @@
+"use strict"; // Treat all js code as newer version 
+
+// alert (3+3) We are using node js not browser 
+
+// Code Readability Should be high
